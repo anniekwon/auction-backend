acd094bc76a0c6164f4905bd84502c65d7bd9508
Annie Kwon <annie.s.kwon@gmail.com>
2022-12-16 02:23:25 -0600

    refractor update

--- routes/buyerRoute.js
+++ routes/buyerRoute.js
@@ -3,7 +3,6 @@ const rt = express.Router();
 const Product = require('../models/productModel')
 const Buyer = require('../models/buyerModel');
 const Bid = require('../models/bidModel');
-const { restart } = require('nodemon');
 
 //PLACES A BID ON AN UNIQUE PRODUCT AS A BUYER
 rt.post('/place-bid', async (req, res) => {
@@ -48,11 +47,18 @@ rt.patch('/update-bid/:productId/:buyerEmailId/:newBidAmount', async (req, res) 
     try {
         const query = { productId: req.params.productId, email: req.params.buyerEmailId }
         const newBid = { bidAmount: req.params.newBidAmount }
-        const product = res.product
-        const buyer = await Bid.findOneAndUpdate(query, newBid)
 
-        res.json(buyer)
+        const oldBid = await Bid.find(query)
 
+        if(req.params.newBidAmount > oldBid[0].bidAmount) {
+            const buyer = await Bid.findOneAndUpdate(query, newBid)
+            res.status(201).json(buyer)
+        } else {
+            res.send({ message: "Your bid is too low. Must be higher than " + oldBid[0].bidAmount})
+        }
+        
+
+        
     } catch(err) {
         res.status(400).json({ message: err.message })
     }
